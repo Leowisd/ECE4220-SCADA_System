0a7a0204016ba587034a9fdd3886d6bcb0df0cb9
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2018-05-01 18:12:12 -0500

    Update historian.c

--- historian.c
+++ historian.c
@@ -30,7 +30,8 @@ void printHistory();
 char buffer[MSG_SIZE],ip[MSG_SIZE],receivedIP[MSG_SIZE], ipHolder[MSG_SIZE];
 char broadcastMSG[MSG_SIZE];
 
-int sock, length, n, r;
+int sock, n, r;
+unsigned int length;
 int boolval = 1;		// for a socket option
 // Use this to find IP
 struct ifreq ifr;
@@ -40,7 +41,7 @@ struct sockaddr_in server;
 struct sockaddr_in from; // From the client
 int port_number;
 int flagg = 0;
-
+struct hostent *hp;
 typedef struct {
     char* time;
 	char* switchStatus;
@@ -49,6 +50,21 @@ typedef struct {
 	char* adcValue;
 	char* event;
 }logList; 
+
+int parseIP(char* IP)
+{
+    char *saveptr;
+    char* temp = strtok_r(IP, ".", &saveptr);
+    int i = 1, numBoard = 0;
+    while(temp != NULL)
+    {
+        if(i == 4)
+            numBoard = atoi(temp);
+        temp = strtok_r(NULL, ".", &saveptr);
+        i++;
+    }
+    return numBoard;
+}
 
 void error(const char *msg)
 {
@@ -61,72 +77,54 @@ logList allLogs[1000];
 
 void sendCommand();
 void printStatus();
-int main(void){
-	int option;
+
+int main(int argc, char *argv[]){
 	int option;
 	pthread_t sender, reader;
 	
 	
-	if (argc == 2){
+    if (argc == 2){
         port_number = atoi(argv[1]);
     } else {
         port_number = 2000; // set default if not provided
     }
 
-	// Creates socket. Connectionless.
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0){
-        error("Opening socket");
-	}
-
-    length = sizeof(server);                // length of structure
-    bzero(&server,length);                  // sets all values to zero. memset() could be used
-    server.sin_family = AF_INET;            // symbol constant for Internet domain
-    server.sin_port = htons(port_number);	// port number
-    server.sin_addr.s_addr = INADDR_ANY;	// IP address of the machine on which the server is running
-
-
-
-    // gets the host name and the IP
-	bzero(&ifr, sizeof(ifr));	// Set all values to zero
-    ifr.ifr_addr.sa_family = AF_INET;	// Type of address to retrieve - IPv4 IP address
-    strncpy((char* )&ifr.ifr_name , eth0 , IFNAMSIZ-1);		// Copy the interface name in the ifreq structure
-	// Get IP address
-    if (ioctl(sock,SIOCGIFADDR,&ifr) == -1) {
-		error("Cannot get IP address");
-		close(sock);
-		exit(0);
-	}
-	// Converts the internet host address in network byte order to a string in IPv4 dotted-decimal notation
-    strcpy(ip,inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
-    strcpy(ipHolder,ip);
-    int current_board_number = parseIP(ipHolder);
-    printf("IP : %s\n",ip);
-    printf("Board Number : %d\n",current_board_number);
-
-
-    // binds the socket to the address of the host and the port number
-    if (bind(sock, (struct sockaddr *)&server, length) < 0){
-        error("binding");
-	}
-    // set broadcast option
+    sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
+    if (sock < 0)
+        error("socket");
+    
+    // change socket permissions to allow broadcast
     if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
-   	{
+    {
         printf("error setting socket options\n");
         exit(-1);
-   	}
-    fromlen = sizeof(struct sockaddr_in);	// size of structure
-	
-	
+    }
+
+    length = sizeof(server); // determines lenght of the structure
+    bzero(&server, length); // set all valus = 0
+    server.sin_family = AF_INET; //constant for internet domain
+    server.sin_addr.s_addr= INADDR_ANY; // MY IP address
+    server.sin_port = htons(port_number);
+
+    
+    if (bind(sock, (struct sockaddr *)&server, length) < 0){
+        printf("binding error dumby\n");
+    }
+    
+    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0){
+        printf("error setting socket option\n");
+        exit(-1);
+	} 
+	fromlen = sizeof(struct sockaddr_in);
+
+
 	sem_init(&sem, 0, 1);
+
 	//Start the pthreads
-	//pthread_create(&sender, NULL, sendMessages, NULL);
 	pthread_create(&reader, NULL, readMessages, NULL);
 	while(1){
 		displayChoice();
-		scanf("%d",option);
-	
-		handleUserInput(option);
+		scanf("%d",&option);
 		switch(option){
 			case 1:
 				sendCommand();
@@ -152,12 +150,10 @@ void printStatus(){
 
 void sendCommand(){
 	bzero(broadcastMSG, MSG_SIZE);
-	
 	printf("Command option:\n LED1ON\n LED2ON \n LED3ON \n LED1OFF\n LED2OFF \n LED3OFF \n Please enter the command:");
 	scanf("%s",broadcastMSG);
-	
-	from.sin_addr.s_addr = inet_addr("128.206.19.255");
-	n = sendto(sock, broadcastMSG, MSG_SIZE, 0, (struct sockaddr *)&from, fromlen);
+	server.sin_addr.s_addr = inet_addr("128.206.19.255");
+	n = sendto(sock, broadcastMSG, MSG_SIZE, 0, (const struct sockaddr *)&server,fromlen);
 }
 
 void* readMessages(void* agr){
@@ -165,12 +161,18 @@ void* readMessages(void* agr){
 	while(1){
 		logList log;
 		bzero(&buffer,MSG_SIZE); // clear the buffer to NULL
+		
 		n = recvfrom(sock, buffer, MSG_SIZE, 0, (struct sockaddr *)&from, &fromlen);
-		if(!strncmp(buffer, "LED1ON", 6) || !strncmp(buffer, "LED2ON", 6) || !strncmp(buffer, "LED3ON", 6)){}
-		else if(!strncmp(buffer, "LED1OFF", 6) || !strncmp(buffer, "LED2OFF", 6) || !strncmp(buffer, "LED3OFF", 6)){}
-		else{
+
+		//if(!strncmp(buffer, "LED1ON", 6) || !strncmp(buffer, "LED2ON", 6) || !strncmp(buffer, "LED3ON", 6)){}
+		//else if(!strncmp(buffer, "LED1OFF", 6) || !strncmp(buffer, "LED2OFF", 6) || !strncmp(buffer, "LED3OFF", 6)){}
+
 			printf("%s\n",buffer);
-			if(count == 1){
+
+			//printf("******\n");
+
+
+			/*if(count == 1){
 				log.time = buffer;
 			}
 			else if(count == 2){
@@ -192,18 +194,16 @@ void* readMessages(void* agr){
 			sem_wait(&sem);
 				allLogs[i] = log;
 				allLogs[i+1] = NULL;
-			sem_post(&sem);
-		}
-		count++;
+			sem_post(&sem);*/
 	}
 }
 
-logList *sortLogEntry(char* buffer){
+/*logList *sortLogEntry(char* buffer){
 	
 	logList localLogs[1000];
 	
 	sem_wait(&sem);
-		memcpy(localLogs,allog, sizeof(logList));
+		memcpy(localLogs,allLogs, sizeof(logList));
 	sem_post(&sem);
 	
 	
@@ -218,4 +218,4 @@ logList *sortLogEntry(char* buffer){
      
 	
 	return log;
-}
+}*/
